4ab69330851727c0b189e467ac37d2ab9c190f9d
jenskragelund <jenskragelund@gmail.com>
2022-04-03 10:47:39 +0200

    2 files modified, 2 added, 1 deleted

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
-const express = require('express');
+import path from 'path';
+import express from 'express';
 const app = express();
 const port = 3010;
-const path = require('path');
 
 app.use(express.static('static'));
 
